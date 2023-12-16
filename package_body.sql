7890fff43dbae4337ee33f037bcf725417e901f7
MANIKANDAN S <102538684+manikandans007@users.noreply.github.com>
2023-12-16 19:51:53 +0530

    Update package_body.sql

--- package_body.sql
+++ package_body.sql
@@ -1,3 +1,9 @@
+HEADER
+ CREATE PACKAGE cust_sal AS
+ 2 PROCEDURE find_sal(c_id employee.id%type);
+ 3 END cust_sal;
+ 4 /
+BODY
 CREATE OR REPLACE PACKAGE BODY c_sal AS
 PROCEDURE find_sal(c_id employee.id%TYPE) IS
 c_sally employee.salary%TYPE;
@@ -7,3 +13,11 @@ dbms_output.put_output.line('salary of employee:'||c_sally);
 END find_sal;
 END c_sal;
 / 
+
+
+CALLING
+code employee.id%type:=&cc_id;
+  begin
+ cust_sal.find_sal(code);
+ END;
+ /
